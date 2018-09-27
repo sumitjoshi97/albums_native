dc5ce2165a8701b7d7df53d62ebf7f36cb052b64
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-27 23:26:19 +0530

    component to render list of albums

---
+++ src/components/AlbumList/AlbumList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import axios from 'axios'
+
+// import files 
+import Album from './Album/Album'
+
+class AlbumList extends Component {
+    state = { albums: [] }
+
+    componentDidMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(res => this.setState({ albums: res.data }))
+    }
+
+    // function to map albums array to separate album component
+    renderAlbums() {
+        return this.state.albums.map(album => 
+            <Album 
+                key={album.title}
+                albumTitle={album.title} 
+                albumArtist={album.artist}
+                albumImg={album.image}
+                albumUrl={album.url}
+            />);
+    }
+
+    // rendering the albumlist component
+    render() {
+        return (
+            <View>
+                {this.renderAlbums()}
+                {console.log(this.state.albums)}
+            </View>
+        )
+    }
+}
+
+export default AlbumList
