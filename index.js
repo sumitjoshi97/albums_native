faffba1c81ee7aba806e3048cf8e2ff7aeab709b
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-28 12:00:30 +0530

    refactored

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 //import librarries
-import React from 'react'
-import { View, AppRegistry } from 'react-native'
-import { name as appName } from './app.json';
+import React from "react";
+import { View, AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
 
-import Header from './src/components/Header'
-import AlbumList from './src/components/AlbumList/AlbumList';
+import Header from "./src/components/Header";
+import AlbumList from "./src/components/AlbumList/AlbumList";
 
 //create component
 const App = () => (
-    <View>
-        <Header headerText='Albums' />
-        <AlbumList />
-    </View>
-
-)
-
+  <View style={{ flex: 1 }}>
+    <Header headerText="Albums" />
+    <AlbumList />
+  </View>
+);
 
 //render compoenent
 AppRegistry.registerComponent(appName, () => App);
